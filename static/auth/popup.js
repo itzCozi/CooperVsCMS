251799b3affcbefe21e31954610125379530729b
Cooper <Cooperransom08@gmail.com>
2023-12-10 10:51:35 -0500

    update login page

--- static/auth/popup.js
+++ static/auth/popup.js
@@ -7,3 +7,14 @@ function hideErrorPopup() {
   // Hide invalid credentials popup
   document.getElementById("invalid-credentials-popup").style.display = "none";
 }
+
+function displayWorkingPopup() {
+  // Shows valid credentials popup
+  document.getElementById("valid-credentials-popup").style.display = "block";
+}
+
+function hideWorkingPopup() {
+  // Hide valid credentials popup
+  document.getElementById("valid-credentials-popup").style.display = "none";
+  window.location = "../index.html";
+}
