d3f9f05d9f3a1408bf826b988f467b9da376df15
Cooper <Cooperransom08@gmail.com>
2023-12-14 13:57:51 -0500

    Run auth-script before link access to uv/service

--- static/uv/sw.js
+++ static/uv/sw.js
@@ -1,52 +1,10 @@
-// Check user auth if user is logged in or not
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
-import {
-  getAnalytics
-} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
-import {
-  getAuth,
-  setPersistence,
-  browserSessionPersistence,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth.js";
-
-// Initialize Firebase with your config
-const firebaseConfig = {
-  apiKey: "AIzaSyDCdqFYJy9aXN36hTNNdA-1Ks1oPZj3gE0",
-  authDomain: "coopervscms.firebaseapp.com",
-  projectId: "coopervscms",
-  storageBucket: "coopervscms.appspot.com",
-  messagingSenderId: "406730745781",
-  appId: "1:406730745781:web:7b537e28c024d898361bfc",
-  measurementId: "G-GTTF3XVXW0",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-// If some silly billy changes the address bar to index.html without completing login
-setPersistence(auth, browserSessionPersistence)
-  .then(() => {
-    // Now you can use onAuthStateChanged in the entire application
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log('User is signed in: ', user.uid);
-      } else {
-        console.log('Your not crafty bro...');
-        window.location.href = "auth/login-page.html";
-      }
-    });
-  })
-  .catch((error) => {
-    console.error('Error setting persistence:', error);
-  });
+// Run auth-script before link access
+var auth_script = require("../auth/auth-script.js");
+auth_script.auth_function();
 
 importScripts('/uv/uv.sw.js');
 const sw = new UVServiceWorker();
+
 self.addEventListener('fetch', event => {
 	event.respondWith(sw.fetch(event));
 });
