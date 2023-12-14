f0965ad7ebfbada569ba4632fb42d035c0e4e314
Cooper <Cooperransom08@gmail.com>
2023-12-14 13:35:35 -0500

    add a console log to sw.js for debugging

--- static/uv/sw.js
+++ static/uv/sw.js
@@ -1,6 +1,8 @@
 importScripts('/uv/uv.sw.js');
 const sw = new UVServiceWorker();
 
+console.log('uv/service called')
+
 self.addEventListener('fetch', event => {
 	event.respondWith(sw.fetch(event));
 });
