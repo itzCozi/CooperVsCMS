a7c607a0b69a77f580ac049a198fc5efea4198dc
Cooper <Cooperransom08@gmail.com>
2023-12-14 08:22:07 -0500

    add comment

--- static/uv/sw.js
+++ static/uv/sw.js
@@ -1,6 +1,8 @@
 importScripts('/uv/uv.sw.js');
 const sw = new UVServiceWorker();
 
+// This is entry point put auth script here
+
 self.addEventListener('fetch', event => {
 	event.respondWith(sw.fetch(event));
 });
