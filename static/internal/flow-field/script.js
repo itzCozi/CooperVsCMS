49c83891f75fc79d99859a8ae1047034dcdf9598
Cooper <Cooperransom08@gmail.com>
2023-12-14 22:42:00 -0500

    wait i forgot about scroll bars (i hate programming)

--- static/internal/flow-field/script.js
+++ static/internal/flow-field/script.js
@@ -1,4 +1,4 @@
-var points = []
+var points = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight); // Set canvas size to window dimensions
@@ -10,16 +10,15 @@ function setup() {
 
   for (var x = 0; x <= width; x += width / density) {
     for (var y = 0; y <= height; y += width / density) {
-      var p = createVector(x + random(-10, 10), y + random(-10, 10))
-      points.push(p)
+      var p = createVector(x + random(-10, 10), y + random(-10, 10));
+      points.push(p);
     }
   }
 
-  background(10, 20, 30);
+  background(10, 20, 30); // Initial background color
 }
 
 function draw() {
-
   if (frameCount < 400) {
     noStroke();
 
@@ -27,17 +26,16 @@ function draw() {
 
     for (var j = 0; j < 1; j++) {
       for (var i = 0; i < points.length; i++) {
+        var r = map(points[i].x, 0, width, 50, 255);
+        var g = map(points[i].y, 0, height, 255, 50);
+        var b = map(points[i].x, 0, width, 255, 50);
 
-        var r = map(points[i].x, 0, width, 50, 255)
-        var g = map(points[i].y, 0, height, 255, 50)
-        var b = map(points[i].x, 0, width, 255, 50)
+        fill(r, g, b, 10);
 
-        fill(r, g, b, 10)
+        var angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 720);
 
-        var angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 720)
-
-        points[i].add(createVector(cos(angle), sin(angle)))
-        ellipse(points[i].x, points[i].y, 1)
+        points[i].add(createVector(cos(angle), sin(angle)));
+        ellipse(points[i].x, points[i].y, 1);
       }
     }
   } else {
