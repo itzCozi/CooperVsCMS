e1926e54321dd9fd907925f42d48199f99eb7670
Cooper <Cooperransom08@gmail.com>
2024-01-06 19:16:02 +0000

    silly me had a function i needed to call

--- static/user-agent/handler.js
+++ static/user-agent/handler.js
@@ -1,9 +1,7 @@
-function isChromeOS() {
-  const userAgent = navigator.userAgent;
+const userAgent = navigator.userAgent;
 
-  if (userAgent.includes('CrOS')) {
-    window.location = '/static/index.html';
-  } else {
-    window.location = '/static/user-agent/index.html';
-  }
+if (userAgent.includes('CrOS')) {
+  window.location = '/static/index.html';
+} else {
+  window.location = '/static/user-agent/index.html';
 }
