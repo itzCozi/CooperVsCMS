69438e41d1efb98818c15372f3017870ba21412d
Cooper <Cooperransom08@gmail.com>
2024-01-06 20:27:32 +0000

    edit out redirect code and fix index.html rouge text

--- static/user-agent/handler.js
+++ static/user-agent/handler.js
@@ -1,6 +1,11 @@
+// If user-agent is suspected to be ChromeOS the user will be sent back to index.html
+// if not they will continue to be redirected to /user-agent/index.html
+
 if (document.cookie.split(';').some((item) => item.trim().startsWith('handlerVisited='))) {
   console.log('handler.js already executed for this visit.');
-} else {
+}
+
+else {
   console.log('Executing handler.js logic...');
   const userAgent = navigator.userAgent;
 
@@ -8,8 +13,8 @@ if (document.cookie.split(';').some((item) => item.trim().startsWith('handlerVis
     // Set the handlerVisited cookie
     document.cookie = 'handlerVisited=true; path=/';
 
-    //window.location = 'index.html';
+    window.location = 'index.html';
   } else {
-    //window.location = 'user-agent/index.html';
+    window.location = 'user-agent/index.html';
   }
 }
