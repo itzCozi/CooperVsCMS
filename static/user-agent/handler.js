76dc40252a31e84c045bee9c8fa6cb8112c2934c
Cooper <Cooperransom08@gmail.com>
2024-01-06 22:57:03 +0000

    fix script location...

--- static/user-agent/handler.js
+++ static/user-agent/handler.js
@@ -1,6 +1,6 @@
 // If user-agent is suspected to be ChromeOS the user will be sent back to index.html
 // if not they will continue to be redirected to /user-agent/index.html
-var auth = require("/auth/auth-script.js");
+var auth = require("../auth/auth-script.js");
 
 if (document.cookie.split(";").some((item) => item.trim().startsWith("handlerVisited="))) {
   console.log("User is using ChromeOS... ACCESS GRANTED");
