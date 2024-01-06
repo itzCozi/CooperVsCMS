bd52aba2336cc36d7b5a33e3a8e99d6133e9ef07
Cooper <Cooperransom08@gmail.com>
2024-01-06 19:10:54 +0000

    trouble shoot new check

--- static/user-agent/handler.js
+++ static/user-agent/handler.js
@@ -2,8 +2,8 @@ function isChromeOS() {
   const userAgent = navigator.userAgent;
 
   if (userAgent.includes('CrOS')) {
-    window.location = 'index.html';
-  } else {
     window.location = '/index.html';
+  } else {
+    window.location = 'index.html';
   }
 }
