317ad3d6eabe832697d3d6a5ec3ce8962db4a873
Cooper <Cooperransom08@gmail.com>
2024-01-11 16:45:01 -0500

    move user-agent into auth and add api folder for server side auth

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-// Local host / initialization script
-
 import createBareServer from '@tomphttp/bare-server-node';
 import { fileURLToPath } from "url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
+import { json } from "body-parser";
+import admin from 'firebase-admin';
+import serviceAccount from '/static/api/firebase-adminSDK.json';
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), {
   fallthrough: false
 });
-var server;
-if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
-  server = createHttpsServer({
+const server = existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem") ?
+  createHttpsServer({
     key: readFileSync("../ssl/key.pem"),
     cert: readFileSync("../ssl/cert.pem")
-  });
-} else server = createHttpServer();
+  }) :
+  createHttpServer();
+
+server.use(json());
+
+server.post('/auth', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userCredential = await admin.auth().signInWithEmailAndPassword(email, password);
+    const user = userCredential.user;
+    res.json({ success: true, user });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
 
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) bare.routeRequest(req, res);
@@ -38,18 +55,15 @@ server.on("upgrade", (req, socket, head) => {
 
 server.on('listening', () => {
   const addr = server.address();
-
   console.log(`Server running on port ${addr.port}\n`)
-  /* Code for listing IPS from website-aio */
   console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
   try {
     console.log(`On Your Network: http://localhost:${addr.port}`);
-  } catch (err) {
-    /* Can't find LAN interface */ };
+  } catch (err) {};
   if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(
     `Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
 });
 
 server.listen({
   port: (process.env.PORT || 8080)
-})
+});
