41978af42403dc53b1befe1236c0553d0b757ef6
Cooper <Cooperransom08@gmail.com>
2024-01-18 23:56:01 -0500

    make registration page less shiet

---
+++ static/auth/register/popup.js
@@ -0,0 +1,34 @@
+function displayErrorPopup() {
+  // Shows invalid credentials popup
+  document.getElementById("invalid-input-popup").style.display = "block";
+}
+
+function hideErrorPopup() {
+  // Hides invalid credentials popup
+  document.getElementById("invalid-input-popup").style.display = "none";
+  document.getElementById("email-field").value = "";
+  document.getElementById("password-field").value = "";
+}
+
+function displayInvaildPopup() {
+  // Shows invalid input popup
+  document.getElementById("invalid-credentials-popup").style.display = "block";
+}
+
+function hideInvalidPopup() {
+  // Hide invalid input popup
+  document.getElementById("invalid-credentials-popup").style.display = "none";
+  document.getElementById("email-field").value = "";
+  document.getElementById("password-field").value = "";
+}
+
+function displayWorkingPopup() {
+  // Shows valid credentials popup
+  document.getElementById("invalid-credentials-popup").style.display = "block";
+}
+
+function hideWorkingPopup() {
+  // Hide valid credentials popup
+  document.getElementById("invalid-credentials-popup").style.display = "none";
+  window.location = "../index.html";
+}
