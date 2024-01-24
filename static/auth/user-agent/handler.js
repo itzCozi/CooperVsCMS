80373c8db3bd7add15217e53cb3531067e5e806e
Cooper <Cooperransom08@gmail.com>
2024-01-24 01:15:43 -0500

    this should work now if the user is not at login page or register page execute authFunction()

--- static/auth/user-agent/handler.js
+++ static/auth/user-agent/handler.js
@@ -10,9 +10,9 @@ if (document.cookie.split(";").some((item) => item.trim().startsWith("handlerVis
   if (userAgent.includes("CrOS")) {
     // Set the handlerVisited cookie
     document.cookie = "handlerVisited=true; path=/";
-    if (!window.location.href.includes("/auth/login-page.html") || !window.location.href.includes("/auth/register/register-page.html")) {
-      authFunction(); // This one script handles all user auth including: user-agent and login
-    }
+    if (!(window.location.href.includes("/auth/login-page.html") || window.location.href.includes("/auth/register/register-page.html"))) {
+      authFunction(); // This script handles all user auth, including user-agent and login
+  }
   } else {
     //window.location.href = "/auth/user-agent/index.html";
     console.log("SENT TO BANISHMENT PAGE")
