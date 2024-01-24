5de4fb42ef83ac2d8d88cd5f2e8e180fa71d3753
Cooper <Cooperransom08@gmail.com>
2024-01-24 01:27:08 -0500

    last fix i hope

--- static/auth/user-agent/handler.js
+++ static/auth/user-agent/handler.js
@@ -10,9 +10,11 @@ if (document.cookie.split(";").some((item) => item.trim().startsWith("handlerVis
   if (userAgent.includes("CrOS")) {
     // Set the handlerVisited cookie
     document.cookie = "handlerVisited=true; path=/";
-    if (!(window.location.href.includes("/auth/login-page.html")) || window.location.href.includes("/auth/register/register-page.html")) {
+    if (window.location.href.includes("/auth/login-page.html") || window.location.href.includes("/auth/register/register-page.html")) {
+      console.log("Already on a auth page (either login-page or register-page)")
+    } else {
       authFunction(); // This script handles all user auth, including user-agent and login
-  }
+    }
   } else {
     //window.location.href = "/auth/user-agent/index.html";
     console.log("SENT TO BANISHMENT PAGE")
