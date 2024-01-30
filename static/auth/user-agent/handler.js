8b7b2d9c7287a7dd1d681c2bbeb4494db9e1481e
Cooper Ransom <cooperransom08@outlook.com>
2024-01-30 01:33:50 -0500

    Recode handler.js

--- static/auth/user-agent/handler.js
+++ static/auth/user-agent/handler.js
@@ -1,32 +1,27 @@
 import { authFunction } from "../auth-script.js";
 
-const redirect = false // dont redirect RN
+const redirect = false; // dont redirect RN
 
 const hasRedirected = document.cookie.includes("handlerVisited=true");
+const userAgent = navigator.userAgent;
 
-if (hasRedirected) {
-  console.log("Already redirected in this session.");
-} else {
-  const userAgent = navigator.userAgent;
-
-  if (userAgent.includes("CrOS")) {
-    // Set the handlerVisited cookie
-    document.cookie = "handlerVisited=true; path=/";
-    if (
-      window.location.href.includes("/auth/login-page.html") ||
-      window.location.href.includes("/auth/register/register-page.html") ||
-      window.location.href.includes("/auth/anon/anon-login.html")
-    ) {
-      console.log("Already on an auth page (either login-page or register-page)",);
-    } else {
-      authFunction(); // This script handles all user auth, including user-agent and login
-    }
+if (userAgent.includes("CrOS")) {
+  // Set the handlerVisited cookie
+  document.cookie = "handlerVisited=true; path=/";
+  if (
+    window.location.href.includes("/auth/login-page.html") ||
+    window.location.href.includes("/auth/register/register-page.html") ||
+    window.location.href.includes("/auth/anon/anon-login.html")
+  ) {
+    console.log("Already on an auth page (either login-page or register-page)");
   } else {
-    if (redirect == true) {
-      window.location.href = "/auth/user-agent/index.html";
-      console.log("SENT TO BANISHMENT PAGE");
-    } else {
-      console.log("Redirect overridden");
-    }
+    authFunction(); // This script handles all user auth, including user-agent and login
+  }
+} else {
+  if (redirect == true) {
+    window.location.href = "/auth/user-agent/index.html";
+    console.log("SENT TO BANISHMENT PAGE");
+  } else {
+    console.log("Redirect overridden");
   }
 }
