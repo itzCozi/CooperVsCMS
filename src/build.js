d2630082941be18f4e650b152ddc89b89a85a548
Cooper Ransom <cooperransom08@outlook.com>
2024-02-12 16:27:38 -0500

    make everything epic (prod ready lol)

--- src/build.js
+++ src/build.js
@@ -1,17 +1,21 @@
 import { exec } from "child_process";
-import { dirname, join } from "path";
+import { dirname } from "path";
 import { existsSync, mkdirSync, copyFileSync } from "fs";
+import { startServer } from "./index.js";
 
 // Function to ensure that the output directory exists
 function ensureDirectoryExistence(filePath) {
   const directory = dirname(filePath);
   if (directory !== "" && !existsSync(directory)) {
-    mkdirSync(directory, { recursive: true });
+    mkdirSync(directory, {
+      recursive: true
+    });
   }
 }
 
 // Function to minify HTML
 function minifyHTML(inputFile, outputFile, callback) {
+  console.log(`Minimizing HTML file: ${inputFile}`);
   const tempFile = outputFile + ".temp"; // Temporary file
   copyFileSync(inputFile, tempFile); // Make a copy
   const htmlMinifyCommand = `html-minifier --collapse-whitespace --minify-css true --minify-js true ${tempFile} -o ${outputFile}`;
@@ -22,6 +26,7 @@ function minifyHTML(inputFile, outputFile, callback) {
 
 // Function to minify CSS
 function minifyCSS(inputFile, outputFile, callback) {
+  console.log(`Minimizing CSS file: ${inputFile}`);
   const tempFile = outputFile + ".temp"; // Temporary file
   copyFileSync(inputFile, tempFile); // Make a copy
   const cssMinifyCommand = `uglifycss ${tempFile} > ${outputFile}`;
@@ -32,6 +37,7 @@ function minifyCSS(inputFile, outputFile, callback) {
 
 // Function to minify JavaScript
 function minifyJS(inputFile, outputFile, callback) {
+  console.log(`Minimizing JavaScript file: ${inputFile}`);
   const tempFile = outputFile + ".temp"; // Temporary file
   copyFileSync(inputFile, tempFile); // Make a copy
   const jsMinifyCommand = `uglifyjs ${tempFile} -o ${outputFile}`;
@@ -45,27 +51,28 @@ async function processFiles() {
   const htmlFiles = [ // SO MANY
     "static/internal/extensions/index.html", "static/internal/extensions/marketplace.html", "static/internal/flow-field/index.html",
     "static/internal/newTab/main.html", "static/internal/wiki/index.html", "static/internal/wiki/wiki.html", "static/internal/wiki/eula.html",
-    "static/internal/wiki/philosophy.html", "static/skin/classic/firebug.html", "static/skin/light/firebug.html", "static/skin/xp/firebug.html",
-    "static/index.html", "static/auth/anon/anon-login.html", "static/auth/register/register-page.html", "static/auth/user-agent/index.html", 
-    "static/auth/login-page.html"
+    "static/internal/wiki/philosophy.html", "static/index.html", "static/auth/anon/anon-login.html", "static/auth/register/register-page.html",
+    "static/auth/user-agent/index.html", "static/auth/login-page.html"
   ];
 
   const cssFiles = [
     "static/auth/anon/anon-theme.css", "static/auth/register/register-theme.css", "static/auth/user-agent/style.css", "static/auth/login-theme.css",
-    "static/css/chrome-tabs-dark-theme.css", "static/css/chrome-tabs-dark-theme.styl", "static/css/chrome-tabs.css", "static/css/chrome-tabs.styl", 
-    "static/css/css.css", "static/css/main.css", "static/internal/wiki/wiki-theme.css", "static/skin/classic/firebug.css", "static/skin/light/firebug.css",
-    "static/skin/xp/debugger.css", "static/skin/xp/firebug.css", "static/skin/xp/firebug-1.3a2.css", "static/skin/xp/firebug.IE6.css", "static/skin/xp/html.css"
+    "static/css/chrome-tabs-dark-theme.css", "static/css/chrome-tabs-dark-theme.styl", "static/css/chrome-tabs.css", "static/css/chrome-tabs.styl",
+    "static/css/css.css", "static/css/main.css", "static/internal/wiki/wiki-theme.css", "static/skin/xp/debugger.css", "static/skin/xp/html.css"
   ];
 
   const jsFiles = [
-    "src/build.js", "src/index.js", "static/bare-client.js", "static/inobounce.min.js", "static/auth/auth-script.js", "static/auth/popup.js", 
-    "static/auth/register/popup.js", "static/auth/user-agent/handler.js", "static/fbl/firebug-lite-debug.js", "static/internal/manager.js", 
-    "static/js/bookmarks.js", "static/js/chrome-tabs.js", "static/js/draggabilly.pkgd.min.js", "static/js/extensions.js", "static/js/js.js", 
-    "static/js/xor.js", "static/js/x/core.js", "static/js/x/DarkMode.js", "static/js/x/demoex.js", "static/uv/sw.js", "static/uv/uv.bundle.js", 
-    "static/uv/uv.config.js", "static/uv/uv.handler.js", "static/uv/uv.sw.js"
+    "src/build.js", "src/index.js", "static/bare-client.js", "static/inobounce.min.js", "static/auth/auth-script.js", "static/auth/popup.js",
+    "static/auth/register/popup.js", "static/auth/user-agent/handler.js", "static/internal/manager.js", "static/js/bookmarks.js",
+    "static/js/chrome-tabs.js", "static/js/draggabilly.pkgd.min.js", "static/js/extensions.js", "static/js/js.js", "static/js/xor.js",
+    "static/js/x/core.js", "static/js/x/DarkMode.js", "static/js/x/demoex.js", "static/uv/sw.js", "static/uv/uv.bundle.js", "static/uv/uv.config.js",
+    "static/uv/uv.handler.js", "static/uv/uv.sw.js"
   ];
 
   const startTime = Date.now(); // Start time
+
+  const RED_COLOR = "\x1b[31m"; // ANSI escape code for red color
+  const RESET_COLOR = "\x1b[0m"; // ANSI escape code to reset color
 
   // Process HTML files
   await Promise.all(htmlFiles.map(async (file) => {
@@ -74,11 +81,9 @@ async function processFiles() {
     await new Promise((resolve) => {
       minifyHTML(file, outputFilePath, (error, stdout, stderr) => {
         if (error) {
-          console.error(`HTML Minify Error: ${error.message}`);
+          console.error(`${RED_COLOR}HTML Minify Error: ${error.message}${RESET_COLOR}`);
         } else if (stderr) {
-          console.error(`HTML Minify stderr: ${stderr}`);
-        } else {
-          console.log(`HTML Minify stdout: ${stdout}`);
+          console.error(`${RED_COLOR}HTML Minify stderr: ${stderr}${RESET_COLOR}`);
         }
         resolve();
       });
@@ -92,11 +97,9 @@ async function processFiles() {
     await new Promise((resolve) => {
       minifyCSS(file, outputFilePath, (error, stdout, stderr) => {
         if (error) {
-          console.error(`CSS Minify Error: ${error.message}`);
+          console.error(`${RED_COLOR}CSS Minify Error: ${error.message}${RESET_COLOR}`);
         } else if (stderr) {
-          console.error(`CSS Minify stderr: ${stderr}`);
-        } else {
-          console.log(`CSS Minify stdout: ${stdout}`);
+          console.error(`${RED_COLOR}CSS Minify stderr: ${stderr}${RESET_COLOR}`);
         }
         resolve();
       });
@@ -110,11 +113,9 @@ async function processFiles() {
     await new Promise((resolve) => {
       minifyJS(file, outputFilePath, (error, stdout, stderr) => {
         if (error) {
-          console.error(`JS Minify Error: ${error.message}`);
+          console.error(`${RED_COLOR}JS Minify Error: ${error.message}${RESET_COLOR}`);
         } else if (stderr) {
-          console.error(`JS Minify stderr: ${stderr}`);
-        } else {
-          console.log(`JS Minify stdout: ${stdout}`);
+          console.error(`${RED_COLOR}JS Minify stderr: ${stderr}${RESET_COLOR}`);
         }
         resolve();
       });
@@ -123,7 +124,9 @@ async function processFiles() {
 
   const endTime = Date.now(); // End time
   const elapsedTime = (endTime - startTime) / 1000; // in seconds
-  console.log(`Total time taken: ${elapsedTime} seconds`);
+  console.log(`\nTotal time taken: ${elapsedTime} seconds`);
+  // Start server
+  startServer();
 }
 
 // Entry point
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // Local host / initialization script
-
 import createBareServer from '@tomphttp/bare-server-node';
 import { fileURLToPath } from "url";
 import { createServer as createHttpsServer } from "node:https";
@@ -7,49 +6,59 @@ import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
 
-const bare = createBareServer("/bare/");
-const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), {
-  fallthrough: false
-});
-var server;
-if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
-  server = createHttpsServer({
-    key: readFileSync("../ssl/key.pem"),
-    cert: readFileSync("../ssl/cert.pem")
+export function startServer() {
+  const bare = createBareServer("/bare/");
+  
+  // Determine the directory to serve static files from
+  const staticDirectory = existsSync("../dist") ? "../dist" : "../static";
+
+  const serve = serveStatic(fileURLToPath(new URL(staticDirectory, import.meta.url)), {
+    fallthrough: false
   });
-} else server = createHttpServer();
+  
+  var server;
+  if (existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem")) {
+    server = createHttpsServer({
+      key: readFileSync("../ssl/key.pem"),
+      cert: readFileSync("../ssl/cert.pem")
+    });
+  } else server = createHttpServer();
 
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) bare.routeRequest(req, res);
-  else {
-    serve(req, res, (err) => {
-      res.writeHead(err?.statusCode || 500, null, {
-        "Content-Type": "text/plain",
-      });
-      res.end('Error')
-    })
-  }
-});
+  server.on("request", (req, res) => {
+    if (bare.shouldRoute(req)) bare.routeRequest(req, res);
+    else {
+      serve(req, res, (err) => {
+        res.writeHead(err?.statusCode || 500, null, {
+          "Content-Type": "text/plain",
+        });
+        res.end('Error')
+      })
+    }
+  });
 
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head);
-  else socket.end();
-});
+  server.on("upgrade", (req, socket, head) => {
+    if (bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head);
+    else socket.end();
+  });
 
-server.on('listening', () => {
-  const addr = server.address();
+  server.on('listening', () => {
+    const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}\n`)
-  /* Code for listing IPS from website-aio */
-  console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-  try {
-    console.log(`On Your Network: http://localhost:${addr.port}`);
-  } catch (err) {
-    /* Can't find LAN interface */ };
-  if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(
-    `Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
-});
+    console.log(`Server running on port ${addr.port}\n`)
+    /* Code for listing IPS from website-aio */
+    console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
+    try {
+      console.log(`On Your Network: http://localhost:${addr.port}`);
+    } catch (err) {
+      /* Can't find LAN interface */
+    };
+    if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(
+      `Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+  });
 
-server.listen({
-  port: (process.env.PORT || 8080)
-})
+  server.listen({
+    port: (process.env.PORT || 8080)
+  })
+}
+
+startServer();
